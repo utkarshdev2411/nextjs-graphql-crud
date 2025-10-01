4a1cb5d53d30fb8b59fd938600b1339b061e1193
Utkarsh Sharma <utkarshdev2411@gmail.com>
2025-10-02 00:31:27 +0530

    refactor: clean up BooksResolver by removing unused methods and improving code structure

--- src/books/books.resolver.ts
+++ src/books/books.resolver.ts
@@ -1,14 +1,14 @@
-import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
+// src/books/books.resolver.ts
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { BooksService } from './books.service';
 import { Book } from './entities/book.entity';
 import { CreateBookInput } from './dto/create-book.input';
-import { UpdateBookInput } from './dto/update-book.input';
 
 @Resolver(() => Book)
 export class BooksResolver {
   constructor(private readonly booksService: BooksService) {}
 
-  @Mutation(() => Book)
+  @Mutation(() => Book, { name: 'createBook' })
   createBook(@Args('createBookInput') createBookInput: CreateBookInput) {
     return this.booksService.create(createBookInput);
   }
@@ -16,20 +16,5 @@ export class BooksResolver {
   @Query(() => [Book], { name: 'books' })
   findAll() {
     return this.booksService.findAll();
-  }
-
-  @Query(() => Book, { name: 'book' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.booksService.findOne(id);
-  }
-
-  @Mutation(() => Book)
-  updateBook(@Args('updateBookInput') updateBookInput: UpdateBookInput) {
-    return this.booksService.update(updateBookInput.id, updateBookInput);
-  }
-
-  @Mutation(() => Book)
-  removeBook(@Args('id', { type: () => Int }) id: number) {
-    return this.booksService.remove(id);
   }
 }
