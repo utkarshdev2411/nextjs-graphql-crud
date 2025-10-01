7c67260da5e0f7253c27f61af981338eb933e818
Utkarsh Sharma <utkarshdev2411@gmail.com>
2025-10-02 00:09:06 +0530

    feat: update Books module with validation and new dependencies

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,26 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookInput } from './dto/create-book.input';
 import { UpdateBookInput } from './dto/update-book.input';
+import {Model} from 'mongoose';
+import {InjectModel} from '@nestjs/mongoose';
+import {Book, BookDocument} from './entities/book.entity';
 
 @Injectable()
 export class BooksService {
-  create(createBookInput: CreateBookInput) {
-    return 'This action adds a new book';
-  }
-
-  findAll() {
-    return `This action returns all books`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} book`;
-  }
-
-  update(id: number, updateBookInput: UpdateBookInput) {
-    return `This action updates a #${id} book`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} book`;
-  }
+  constructor(@InjectModel(Book.name) private bookModel: Model<BookDocument>) {}
 }
--- src/books/dto/create-book.input.ts
+++ src/books/dto/create-book.input.ts
@@ -1,7 +1,19 @@
 import { InputType, Int, Field } from '@nestjs/graphql';
+import { IsNotEmpty, IsInt, Min, Max } from 'class-validator';
 
 @InputType()
 export class CreateBookInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field()
+  @IsNotEmpty()
+  title: string;
+
+  @Field()
+  @IsNotEmpty()
+  author: string;
+
+  @Field(() => Int)
+  @IsInt()
+  @Min(1900)
+  @Max(2025)
+  publicationYear: number;
 }
