c5048d410f1bdad0c374c342e365552c89c7236c
Utkarsh Sharma <utkarshdev2411@gmail.com>
2025-10-02 00:15:35 +0530

    feat: implement create and findAll methods in BooksService

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -8,4 +8,16 @@ import {Book, BookDocument} from './entities/book.entity';
 @Injectable()
 export class BooksService {
   constructor(@InjectModel(Book.name) private bookModel: Model<BookDocument>) {}
+
+  //Creating a new book
+  async create(CreateBookInput: CreateBookInput): Promise<Book>{
+    const newBook= new this.bookModel(CreateBookInput);
+    return newBook.save();
+  }
+
+  //RReading all the books
+  async findAll(): Promise<Book[]> {
+    return this.bookModel.find().exec();
+  }
+
 }
